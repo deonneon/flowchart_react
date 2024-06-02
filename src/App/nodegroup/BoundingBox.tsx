171db4228a1ee5407d015b0a126065c495302dd3
deonneon <dtnifti@gmail.com>
2024-06-02 12:37:41 -0500

    update

--- src/App/nodegroup/BoundingBox.tsx
+++ src/App/nodegroup/BoundingBox.tsx
@@ -7,7 +7,6 @@ export type NodeData = {
   label: string;
   sourcePosition?: Position;
   color?: string;
-  style?: React.CSSProperties;
 };
 
 const controlStyle = {
@@ -23,7 +22,7 @@ function ResizeIcon() {
       height="20"
       viewBox="0 0 24 24"
       strokeWidth="2"
-      stroke="blue"
+      stroke="black"
       fill="none"
       strokeLinecap="round"
       strokeLinejoin="round"
