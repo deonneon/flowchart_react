4c08b883938e0d7d734ff85716b245aa56cddac4
deonneon <dtnifti@gmail.com>
2025-03-15 18:50:08 -0500

    improved bounding box perfromance

--- src/App/nodegroup/BoundingBox.tsx
+++ src/App/nodegroup/BoundingBox.tsx
@@ -18,8 +18,8 @@ function ResizeIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
+      width="15"
+      height="15"
       viewBox="0 0 24 24"
       strokeWidth="2"
       stroke="black"
@@ -37,6 +37,29 @@ function ResizeIcon() {
   );
 }
 
+function TrashIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="10"
+      height="10"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="black"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 7h16" />
+      <path d="M10 11v6" />
+      <path d="M14 11v6" />
+      <path d="M5 7l1 14a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -14" />
+      <path d="M9 7v-3h6v3" />
+    </svg>
+  );
+}
+
 const BoundingBoxNode = ({ id, data, selected }: NodeProps<NodeData>) => {
   const deleteNode = useStore((state) => state.deleteNode);
   const setSelectedNodeId = useStore((state) => state.setSelectedNodeId);
@@ -44,44 +67,63 @@ const BoundingBoxNode = ({ id, data, selected }: NodeProps<NodeData>) => {
   return (
     <div
       style={{
-        padding: "5px",
         width: "100%",
         height: "100%",
+        minWidth: "100px",
+        minHeight: "50px",
         cursor: "default",
-        position: "relative", // Add relative positioning to the container
+        position: "relative",
       }}
       onClick={() => setSelectedNodeId(id)}
     >
-      <div className="inputWrapper" style={{ cursor: "grab" }}>
+      {/* Drag Handle */}
+      <div className="inputWrapper" style={{ cursor: "grab", position: "relative" }}>
         <div className="dragHandle">
           <DragIcon />
         </div>
+
+        {/* Toolbar with Delete Button */}
+        {selected && (
+          <div
+            style={{
+              position: "absolute",
+              top: "-30px",
+              // left: "50%",
+              // transform: "translateX(-50%)",
+              display: "flex",
+              // gap: "5px",
+              background: "rgba(255, 255, 255, 0.8)",
+              padding: "1px",
+              borderRadius: "4px",
+              boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.2)",
+              pointerEvents: "auto", // Allows clicking inside the toolbar
+              zIndex: 10, // Ensures it's above other elements
+            }}
+            onMouseDown={(event) => event.stopPropagation()} // Prevent losing selection
+          >
+            <button
+              style={{
+                border: "none",
+                background: "transparent",
+                cursor: "pointer",
+              }}
+              onClick={(event) => {
+
+                deleteNode(id);
+              }}
+            >
+              <TrashIcon />
+            </button>
+          </div>
+        )}
       </div>
+
+      {/* Resize Control */}
       {selected && (
         <NodeResizeControl style={controlStyle} minWidth={100} minHeight={50}>
           <ResizeIcon />
-          <button
-            style={{
-              position: "absolute",
-              borderRadius: "50%",
-              backgroundColor: "pink",
-              border: "0px",
-              right: -10,
-              bottom: -10,
-            }}
-            onClick={() => deleteNode(id)}
-          >
-            -
-          </button>
         </NodeResizeControl>
       )}
-      <div
-        style={{
-          padding: 10,
-          minHeight: 40,
-          minWidth: 100,
-        }}
-      ></div>
     </div>
   );
 };
